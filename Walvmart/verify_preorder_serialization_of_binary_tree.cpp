631671641915f8614920b606bdfcafa12d8cea70
LovishRb <2020010007183@gndu.ac.in>
2025-01-18 01:37:45 +0530

    day 12

---
+++ Walvmart/verify_preorder_serialization_of_binary_tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        stringstream ss(preorder);
+        string curr;
+        int count = 1;
+        while (getline(ss, curr, ',')) {
+            count--;
+            if (count < 0) return false;
+            if (curr != "#") count += 2;
+        }
+        return count == 0;
+    }
+};
